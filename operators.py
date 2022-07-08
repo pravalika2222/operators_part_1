11ee2ea0d565bf4c3cdb497a177ab0fc6ddf33f7
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-07-08 20:54:24 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,57 @@
+'''arithmetic operation'''
+'arithmetic means : {+,-,*,/,%,**] these all are arithmetics operators'
+# a=float(input("enter the value of a:"))    #   2
+# b=float(input("enter the value of b:"))    #   6
+# a,b=[int(x) for x in input('enter the value of a and b:').split()]     #   2 6
+# print(f"the value of {a} + {b} is {a+b}")    #   2+6=8
+# print(f"the value of {a} - {b} is {a-b}")    #   2-6=-4
+# print(f"the value of {a} * {b} is  {a*b}")   #   2*6=12
+# print(f"the value of {a} / {b} is {a/b}")    #   2/6=0.33333333333 (in division answer will be in float)
+# print(f"the value of {a} % {b} is {a%b}")    #   2%6=2
+# print(f"the value of {a} ** {b} is {a**b}")  #   2**6=64(2*2*2*2*2*2=64)
+
+"""floor division"""
+# g=9//3
+# print(g)      #   3
+# d=9//3.0
+# print(d)      #    3.0
+# h=24//2
+# print(h)        #    12
+# e=24//2.0
+# print(e)         #    12.0
+# f=2.8//2
+# print(f)           #   1.0
+
+"""assignment operator"""
+'assignment operator:to assigning the value (or) a statement'
+# a=4 ; b=2
+# a=a+b
+# print(a)      #  a=4+2   a=6
+# a=a+a
+# print(a)      #  a=6+6   a=12
+
+# a=2
+# a+=6
+# # print(a)    #  a=a+6   a=2+6    a=8
+# a-=4
+# # print(a)     #  a=a-4   a=8-4    a=4 
+# a/=2
+# # print(a)      #  a=a/2   a=4/2    a=2.0
+# a*=3
+# # print(a)       # a=a*3    a=2.0*3     a=6.0
+# a**=2
+# print(a)          #  a=a**2   a=6.0**2    a=36.0
+
+
+# p=int(input("enter the value of p:"))   
+# print(f"the value of p is {p}")       # value of p is 2
+# p+=4
+# print("the value of p after p+=4 is {}".format(p))   # the value of p after p+=4 is 8
+# p-=6
+# print("the value of p after p-=6 is {}".format(p))     # the value of p after p-=6 is -4
+# p*=5
+# print("the value of p after p*=5 is {}".format(p))      # the value of p after p*=5 is 10
+# p**=4
+# print("the value of p after p**=4 is {}".format(p))       # the value of p after p**=4 is 16
+# p/=8
+# print("the value of p after p/=8 {}".format(p))           # the value of p after p/=8 is 0.25
